6fcbc2bb91e0b18a771b9c3caee0ab864672be04
Tristan Lynass <tristan@onyxservices.com.au>
2021-07-26 09:25:23 +0800

    Added thumbnail generation and some error handling

--- server-poc/app.js
+++ server-poc/app.js
@@ -4,6 +4,8 @@ const sqlite3 = require('sqlite3')
 const fs = require('fs')
 const fileUpload = require('express-fileupload')
 const sql = require('./sql')
+const imageThumbnail = require('image-thumbnail');
+
 
 const port = 3000
 const db = new sqlite3.Database('data.db', err => err && console.log(err.message));
@@ -18,20 +20,26 @@ app.use(fileUpload({
 app.post('/uploads', async function(req, res) {
   req.files.media.forEach(async file => {
     const filename = `${file.md5}.jpg`;
-    fs.rename(file.tempFilePath, `uploads/${filename}`, () => {
-      // TODO: Generate a thumbnail
-
-      db.run(sql.insert, filename)
+    fs.rename(file.tempFilePath, `uploads/${filename}`, async () => {
+      // Note: fit != inside(not full 200x200), fill(stretches), contains(letterboxes)
+      const options = { width: 200, height: 200, jpegOptions: { force: true, quality: 90 }, fit: 'cover' }
+      const thumbnail = await imageThumbnail(`uploads/${filename}`, options);
+      fs.writeFile(`./uploads/thumbs/${filename}`, thumbnail, () => {
+        db.run(sql.insert, filename)
+      })
     })
 
   })
 
-  // console.log(req.files.media); // the uploaded file object
   res.send();
 });
 
 app.get('/uploads', (req, res) => {
   const page = req.query.page;
+  if (page == null) {
+    return res.status(400).send()
+  }
+
   const pageSize = 50;
   const offset = page * pageSize;
   console.log(sql.getPage, pageSize, offset)
